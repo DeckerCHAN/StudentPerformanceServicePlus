511b57c8cd0b339b74fb9940550737c15e9da8cd
jeffreynam <jeffrey.nam@foxmail.com>
2014-04-19 13:57:14 +0800

    init project structure

---
+++ src/org/sipc/se/servlet/ManagerCenterServlet.java
@@ -0,0 +1,41 @@
+package org.sipc.se.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class ManagerCenterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static int num = 0;
+    /**
+     * Default constructor. 
+     */
+    public ManagerCenterServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.doPost(request, response) ;
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println(num++) ;
+	}
+
+}
