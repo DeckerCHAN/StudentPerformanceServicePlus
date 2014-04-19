38330bfcffe976ea51998115110edc402d7caab1
jeffreynam <jeffrey.nam@foxmail.com>
2014-04-19 22:29:04 +0800

    ...

--- src/org/sipc/se/plugin/Plugin.java
+++ src/org/sipc/se/plugin/Plugin.java
@@ -8,6 +8,6 @@ public interface Plugin {
 	
 	public boolean onEnable() ;
 	
-	public HttpServletResponse getResponse(HttpServletRequest request) ;
+	public HttpServletResponse getResponse(HttpServletRequest request , HttpServletResponse response) ;
 	
 }
