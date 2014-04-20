e5f746a96f3ec0a3ef078cc497fcea00869566b2
jeffreynam <jeffrey.nam@foxmail.com>
2014-04-20 18:01:20 +0800

    Commit Jar File Load

---
+++ src/org/sipc/se/util/JarFileContent.java
@@ -0,0 +1,82 @@
+package org.sipc.se.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+import org.sipc.se.plugin.Plugin;
+
+public class JarFileContent {
+	
+	public static File findJarFile(String filePath) throws IOException{
+		
+		File fileList = new File(filePath) ;
+		
+		if(fileList.exists())
+		{
+			return fileList ;
+		}
+		
+		return null ;
+		
+	}
+	
+	public static String[] getFileList(String filePath) throws IOException{
+		
+		return findJarFile(filePath).list() ;
+		
+	}	
+	
+	public static String getYMLConfig(String fileName) throws IOException{
+		String className = null ;
+		JarFile  jarFile = new JarFile (fileName) ;
+		JarEntry jarEntry = jarFile.getJarEntry( StaticValue.JAR_YMLFILE ) ;
+		
+		//Read .yml File
+		InputStream input = jarFile.getInputStream(jarEntry) ;
+		InputStreamReader inputReader = new InputStreamReader(input) ;
+		BufferedReader reader = new BufferedReader(inputReader) ;
+		
+		//Read
+		String line = null ;
+		while((line = reader.readLine() ) != null){
+			if(line.contains("package")){
+				className = line.split(":")[1].trim() ;
+			}
+		}
+		
+		//Close Readstream
+		reader.close() ;
+		jarFile.close() ;
+		
+		return className ;
+		
+	}
+	
+	public static List<Plugin> getPluginList(String filePath) throws IOException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+		
+		File fileList = findJarFile(filePath) ;
+		List<Plugin> pluginList = new ArrayList<Plugin>() ;
+		
+		for(String fileName : fileList.list() ){
+			
+			String className = getYMLConfig(filePath + "/" + fileName ) ;
+			
+			//Load Plugin Instance
+			Plugin plugin = JarFileLoad.jarFileLoad( filePath, fileName , className) ;
+			
+			//Add All Plugin Into List
+			if( plugin.onEnable() ) {
+				pluginList.add(plugin) ;
+			}
+		}
+		return pluginList ;
+	}
+	
+}
---
+++ src/org/sipc/se/util/JarFileLoad.java
@@ -0,0 +1,47 @@
+package org.sipc.se.util;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import org.sipc.se.plugin.Plugin;
+
+
+class JarLoader extends URLClassLoader {
+	
+	private static JarLoader loader = null;
+	
+	private JarLoader(){
+		super(new URL[0], JarLoader.class.getClassLoader());
+	}
+	public static JarLoader getInstance(){
+		if(loader==null)
+			loader = new JarLoader();
+		return loader;
+	}
+	public void addURL(String url) throws MalformedURLException {
+
+		this.addURL(new URL(url));
+	}
+
+}
+
+public class JarFileLoad{
+	
+	public static Plugin jarFileLoad(String pluginPath , String plginName , String classFullName) throws MalformedURLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+		
+		JarLoader loader = JarLoader.getInstance();
+		
+		//Load yourProject/Plugin/*.jar
+		loader.addURL("jar:file:///" + pluginPath + "/" + plginName + "!/");
+		//New Instance
+		
+		try{
+			Plugin plugin = (Plugin)Class.forName( classFullName , true, loader).newInstance();
+			return plugin ;
+		}catch(Exception e){
+			return null ;
+		}
+		
+	}
+}
